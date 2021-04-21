9c92dc995b1b44a39b7c2e840f40a17561ca8a75
Amit Chaudhari <amitchaudhari9121@gmail.com>
2021-04-21 11:10:23 +0530

    The Road to strict:"true" Added declarations

---
+++ src/types/declarations.d.ts
@@ -0,0 +1,9 @@
+// This contains the type declarations for all those pure JS modules, which don't have types
+declare module "react-dark-mode-toggle";
+declare module "postcss";
+declare module "next-pwa";
+declare module "lottie-react-web";
+declare module "countapi-js";
+declare module "@svgr/webpack";
+declare module "postcss-nesting";
+declare module "babel-plugin-styled-components";
