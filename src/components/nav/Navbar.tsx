16cbbc430a052814f94e8611b1c149b1aa430642
Amit Chaudhari <amitchaudhari9121@gmail.com>
2021-05-15 15:45:11 +0530

    Assets updated.

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -5,39 +5,49 @@ import { Discover, Genre } from "../../utils/data";
 import Toggle from "./Toggle";
 import { useSelector } from "react-redux";
 import State from "../../types/state";
+
 const Navbar = ({ visit }: any) => {
-  const { theme } = useSelector((state: State) => state);
-  return (
-    <div
-      className={`flex flex-col w-2/12 ${theme.background} ${theme.border.notselected} border-r justify-start items-center border-opacity-50 hidden lg:flex `}
-    >
-      <Link href="/recentlyadded/1">
-        <div
-          className={`${theme.text.selected}  cursor-pointer text-4xl  justify-center flex-col  flex items-center text-center`}
-        >
-          <Image
-            width={300}
-            height={250}
-            src={
-              theme.theme == "dark"
-                ? "/animexlogodark.svg"
-                : "/animexlogolight.svg"
-            }
-          />
-        </div>
-      </Link>
-      <Toggle />
-      <NavContainer links={Discover} heading={"Discover"} />
-      <NavContainer links={Genre} heading={"Genres"} />
-      <span
-        className={`${theme.text.notselected} text-lg px-10  w-full justify-start  flex items-center `}
-      >
-        Visits:&nbsp;
-        <span className={`${theme.text.selected} text-xl font-bold`}>
-          {visit}
-        </span>
-      </span>
-    </div>
-  );
+	const { theme } = useSelector((state: State) => state);
+	return (
+		<div
+			className={`flex 
+      flex-col 
+      w-2/12 
+      ${theme.background} 
+      ${theme.border.notselected} 
+      border-r justify-start 
+      items-center 
+      border-opacity-50 
+      hidden 
+      lg:flex `}
+		>
+			<Link href="/recentlyadded/1">
+				<div
+					className={`${theme.text.selected}  cursor-pointer text-4xl  justify-center flex-col  flex items-center text-center`}
+				>
+					<Image
+						width={400}
+						height={250}
+						src={
+							theme.theme == "dark"
+								? "/anime-jikan-logo-dark.svg"
+								: "/anime-jikan-logo-light.svg"
+						}
+					/>
+				</div>
+			</Link>
+			<Toggle />
+			<NavContainer links={Discover} heading={"Discover"} />
+			<NavContainer links={Genre} heading={"Genres"} />
+			<span
+				className={`${theme.text.notselected} text-lg px-10  w-full justify-start  flex items-center `}
+			>
+				Visits:&nbsp;
+				<span className={`${theme.text.selected} text-xl font-bold`}>
+					{visit}
+				</span>
+			</span>
+		</div>
+	);
 };
 export default Navbar;
